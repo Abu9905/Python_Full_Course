abd069582b6a643c4b487981d1965da812defb15
Abu shama <mabu53140@gmail.com>
2025-05-27 08:59:45 +0530

    Ep-01

---
+++ 01_Setup/syntax.py
@@ -0,0 +1,13 @@
+
+print("Hello Abu !!")
+print("Hello There ! How are U ??")
+print("I am Good !")
+print("How are You !!!")
+print(3)
+
+a = 1
+
+if(a>3):
+    print("true!!")
+else:
+    print("flase")
