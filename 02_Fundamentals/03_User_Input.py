da45830370e19829d5fbc227b89e3662ca4a245d
Abu shama <mabu53140@gmail.com>
2025-05-28 20:45:53 +0530

    User_Input

---
+++ 02_Fundamentals/03_User_Input.py
@@ -0,0 +1,24 @@
+
+# ======= User Input ======= //
+# The input() function is used to take input from the user.
+# The input is always returned as a string, so you may need to convert it to the appropriate type.
+# Example: Taking user input and converting it to an integer
+# Taking user input
+
+# user_input = input("Enter your age: ")
+# Displaying the input8
+# print("You entered:", user_input)
+
+
+# ========== Add two Information ========== //
+
+# a = input("Enter First Number: ")
+# a = int(a)
+
+# # Displaying the information78
+# print( a + 8)
+
+a = input("Enter First Number: ")
+b = input("Enter Second Number: ")
+
+print("The sum of the two numbers is:", int(a) + int(b))
