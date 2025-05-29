30c52010833b07c42bc9d73739cf022b1bd16b11
Abu shama <mabu53140@gmail.com>
2025-05-29 11:51:25 +0530

    Operators

---
+++ 02_Fundamentals/05_Operators.py
@@ -0,0 +1,84 @@
+
+# / ======= Operators ======= /
+'''
+
+# 1.) Arithmetic Operators:-
+#    +, -, *, /, //, %, **
+
+num1 = 10;
+num2 = 24;
+
+print("Addition:", num1 + num2)  # Addition
+print("Subtraction:", num1 - num2)  # Subtraction
+print("Multiplication:", num1 * num2)  # Multiplication
+print("Division:", num1 / num2)  # Division
+print("Floor Division:", num1 // num2)  # Floor Division
+print("Modulus:", num1 % num2)  # Modulus
+print("Floor Division:", num1 // num2, )  # Floor Division with message
+print("Exponentiation:", num1 ** num2)  # Exponentiation
+'''
+
+'''
+# 2.) Comparison Operators:-
+#    ==, !=, >, <, >=, <=
+
+a = 21;
+b = 21;
+print("a == b:", a == b)  # Equal to
+print("a != b:", a != b)  # Not equal to
+print("a > b:", a > b)  # Greater than
+print("a < b:", a < b)  # Less than
+print("a >= b:", a >= b)  # Greater than or equal to
+print("a <= b:", a <= b)  # Less than or equal to
+'''
+
+'''
+# 3.) Logical Operators:-
+#    and, or, not
+
+a = True
+b = False
+print("a and b:", a and b)  # Logical AND
+print("a or b:", a or b)  # Logical OR
+print("not a:", not a)  # Logical NOT
+'''
+
+'''
+# 4.) Assignment Operators:-
+#    =, +=, -=, *=, /=, //=, %=, **=
+
+a = 4; 
+
+a+= 2  # Add and assign
+print("a += 2:", a)  # a = a + 2
+a -= 2  # Subtract and assign
+print("a -= 2:", a)  # a = a - 2
+a *= 2  # Multiply and assign
+print("a *= 2:", a)  # a = a * 2
+a /= 2  # Divide and assign
+print("a /= 2:", a)  # a = a / 2
+a //= 2  # Floor divide and assign
+print("a //= 2:", a)  # a = a // 2
+a %= 2  # Modulus and assign
+print("a %= 2:", a)  # a = a % 2
+a **= 2  # Exponentiate and assign
+print("a **= 2:", a)  # a = a ** 2
+'''
+
+'''
+# 5.) Membership Operators:-
+#    in, not in
+
+my_list = [1, 2, 3, 4, 5]
+print("1 in my_list:", 1 in my_list)  # Check if 1 is in the list
+print("6 not in my_list:", 6 not in my_list)  # Check if 6 is not in the list
+'''
+
+'''
+# 6.) Identity Operators:-
+#    is, is not
+x = 110;
+y = 10;
+print("x is y:", x is y)  # Check if x and y are the same object
+print("x is not y:", x is not y)  # Check if x and y are not the same object
+'''
