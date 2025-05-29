63809834d0f92bd5655ec028c7f5f044f46a3c07
Abu shama <mabu53140@gmail.com>
2025-05-29 09:54:29 +0530

    Comments , Escape Sequence and Print Statement

---
+++ 02_Fundamentals/04_Comments_Print_Esc.py
@@ -0,0 +1,23 @@
+
+# ====== Comments , Escape Sequence and Print Statement ====== //
+# Single-line comments start with a hash (#)
+# Multi-line comments can be done with triple quotes (''' or """)
+
+#  This is a single-line comment
+
+'''
+print("Hello world!!)
+   This is a multi-line comment.
+'''
+
+
+# 2.) Escape Sequence:-
+# print("Hy How are u \nI am fine !!\\newLine")
+
+# print("Hello \" World")
+
+
+# === 3.) Print Statement === //
+print("Hello World")
+print("Hello World", "Abu",5)
+print("Hello World", "How are you?", sep="/", end="\n")
